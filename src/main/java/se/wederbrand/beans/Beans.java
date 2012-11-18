caa3b939e02904ddd3b00e981565d9dfbdeb4ff6
Andreas Wederbrand <andreas@wederbrand.se>
2012-11-18 07:11:23 +0100

    minor change

--- src/main/java/se/wederbrand/beans/Beans.java
+++ src/main/java/se/wederbrand/beans/Beans.java
@@ -3,6 +3,9 @@ package se.wederbrand.beans;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * just testing stuff
+ */
 public class Beans {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
